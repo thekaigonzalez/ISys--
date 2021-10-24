c82d837f4356c51356e83ea31c4275dd88d61508
Kai D. Gonzalez <gkai70264@gmail.com>
2021-10-23 18:34:49 -0700

    `trim` is now defined in newlexer.h

--- isys/stat.h
+++ isys/stat.h
@@ -26,24 +26,6 @@ int getLastIndex(const std::string &str, char ch) {
    return -1;
 }
 
-const std::string WHITESPACE = " \n\r\t\f\v";
- 
-std::string ltrim(const std::string &s)
-{
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
-}
- 
-std::string rtrim(const std::string &s)
-{
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
- 
-std::string trim(const std::string &s) {
-    return rtrim(ltrim(s));
-}
-
 std::map<std::string, std::string> varmem;
 
 
